695412dd015cbb0742e8e1080b346c0ce7863095
sparklingwaterlemon <michaelkim009@gmail.com>
2022-10-31 17:45:06 -0700

    basic CSS layout for Hero Section

---
+++ src/Assets/Images/index.jsx
@@ -0,0 +1,10 @@
+
+const HEROIMAGES = [
+    {"img": require("./1XL.jpg")},
+    {"img": require("./2XL.jpg")},
+    {"img": require("./3XL.jpg")},
+    {"img": require("./4XL.jpg")},
+    {"img": require("./5XL.jpg")},
+]
+
+export default HEROIMAGES
--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,16 +1,20 @@
 import "./HeroSection.css";
+import HEROIMAGES from "../../Assets/Images/index";
 
 export default function HeroSection(){
     return (
         <section id="hero">
-            <div className="hero">
-                <h2> image </h2>
-                <h2> Name </h2>
+            <div className="hero-image-container">
+                {/* <img src={HEROIMAGES[0].img} className="hero-image"/> */}
+                <img src="" className="hero-image"/>
+                <div className="hero-name">
+                    Michael Kim
+                </div>
             </div>
-            <h2> hero </h2>
-            <ul>
-                <li> hero </li>
-            </ul>
+            <div className="hero-pb-container">
+                <div className="hero-pb"> Personal Brand Statement Header Personal Brand Statement Header Personal Brand Statement Header</div>
+            </div>
+            <div className="pb-text"> blah bloo beeh happy halloween blah bloo beeh happy halloween  blah bloo beeh happy halloween blah bloo beeh happy halloween blah bloo beeh happy halloween blah bloo beeh happy halloween blah bloo beeh happy halloween blah bloo beeh happy halloween s</div>
         </section>
     )
 }
