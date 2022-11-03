c46f6896810320f5006801c55aea765541cf99c0
sparklingwaterlemon <michaelkim009@gmail.com>
2022-11-03 14:25:34 -0700

    NavBar change color function roughed out- revisited hero css

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,17 +1,36 @@
 import "./NavBar.css";
 import { useState } from "react";
 import { Link } from "react-scroll";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 export default function NavBar() {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
 
   const toggle = () => {
     setIsNavExpanded(!isNavExpanded)
-  }
+  };
+  
+
+  // changing Nav Bar Color
+  // Probably have to change to a useRef from ternary operation..
+  const [navBarColor, setNavBarColor] = useState(false);
+  const changeNav = () => {
+    if(window.scrollY > 2000){
+      setNavBarColor(true)
+    } else {
+      setNavBarColor(false)
+    }
+  };
+  useEffect(()=>{
+    window.addEventListener("scroll", changeNav)
+  });
+
 
   return (
     
       <nav className="nav-bar">
+      {/* <nav className={navBarColor ? "nav-bar change" : "nav-bar"}> */}
         
         <span className="nav-home" onClick={() => window.scrollTo({top: 0,left: 0,})}>
           Mike
