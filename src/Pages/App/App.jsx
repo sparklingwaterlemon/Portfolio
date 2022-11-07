341a5aba0c5976c099c9f703459f95b9c91a723e
sparklingwaterlemon <michaelkim009@gmail.com>
2022-11-06 23:19:13 -0800

    N5 - About

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
     <>
       {/* <NavBar /> */}
       <HeroSection />
-      {/* <AboutSection /> */}
+      <AboutSection />
 
 
       {/* <ProjectSectionA /> */}
