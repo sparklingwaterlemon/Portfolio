332e4e976950a00b9c387d220f62117bd34fb83a
sparklingwaterlemon <michaelkim009@gmail.com>
2022-11-29 21:01:41 -0800

    WIP - About Section

--- src/Components/AboutSection/AboutSection.jsx
+++ src/Components/AboutSection/AboutSection.jsx
@@ -1,4 +1,6 @@
-import "./AboutSection.css"
+import "./AboutSection.css";
+import { Link } from "react-scroll";
+
 
 export default function AboutSection(){
     return(
@@ -7,25 +9,32 @@ export default function AboutSection(){
                 <div className="about-text">
 
                     <p className="at-p"> 
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam voluptas ipsam 
-                    culpa labore maiores enim voluptatem, voluptatibus deserunt illo nemo reprehenderit 
-                    nesciunt ipsum consectetur hic delectus! Nulla voluptatibus perspiciatis odio. Lorem ipsum, 
-                    dolor sit amet consectetur adipisicing elit. Magnam alias hic eaque, commodi rerum ea, iure 
-                    repellendus totam repudiandae quidem iste quasi est illo autem eius. Nihil non facere sit.
+                    In search of a Forever Career - My journey from Ceramic Artist to Software Engineer
+                    <br/> (Skip ahead to <Link id="skip-to-project" activeClass="active" smooth spy to="projects">Projects</Link>)
                     <br/>
                     <br/>
-   
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur iure fugiat aliquam autem minus 
-                    incidunt! Pariatur architecto, necessitatibus 
-                    adipisci vero doloremque debitis molestiae iusto perspiciatis, officiis in magnam deserunt consequuntur. Lorem 
-                    ipsum, dolor sit amet consectetur adipisicing elit. Tenetur maxime magni modi quibusdam blanditiis totam porro 
-                    cumque molestias ratione temporibus. Repellendus, explicabo doloremque! Ad saepe amet tempore cum distinctio officiis.
+                    <ul> 2009 - 2013
+                        <li>Graduated from Centre College in 2013 with a B.A in Studio Art and a minor in Chemistry.</li>
+                        <li>Cofounded the Iota Chapter of the Delta Kappa Epsilon Fraternity and served as the Interfraternity Council Delegate.</li>
+                        <li>Served as the Men's Lacrosse Team Student Manager.</li>
+                    </ul>
                     <br/>
+                    <ul> 2013 - 2015
+                        <li>Started Pallas! Ceramics. Pursued a career as a Ceramicist, with a focus on nonfunctional art.</li>
+                        <li>Biggest failure and a bigger lesson learned!</li>
                     <br/>
-                    
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quaerat omnis excepturi iusto 
-                    delectus asperiores, ratione aut iste, odit quasi dignissimos? Nostrum doloribus mollitia,
-                    provident aperiam libero officia similique voluptatum!
+                        <li>While pursuing ceramics, I became a bartender to support myself.</li>
+                        <li>Attended a bartending school, bartended at Xlanes LA, and eventually at the prestigous Bel-Air Bay Club.</li>
+                    </ul>
+
+
+
+                    <br/> Approx. 2015 - 2017
+                    <br/> - With my mother's advice, who worked at a bank for 20+ years, decided to work in a traditional office setting.
+                    <br/> - Became a Billing Assistant at an ABA start up, Behavior Frontiers, and quickly became a Billing Specialist.
+                    <br/> - After the 2 years, I've decided to pursue a different 
+                
+
                     </p>
                 </div>
             </div>
