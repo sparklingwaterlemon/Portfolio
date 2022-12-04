2dd3d77a3fb4ada6d705b1522c739be6d640276c
sparklingwaterlemon <michaelkim009@gmail.com>
2022-12-03 20:42:31 -0800

    ProjectSectionB complete - CSS for both mobile and desktop. Adjusted CSS for ProjectSectionA as well

--- src/Components/ProjectSectionA/ProjectSectionA.jsx
+++ src/Components/ProjectSectionA/ProjectSectionA.jsx
@@ -4,28 +4,28 @@ export default function ProjectSectionA(){
     return(
         <section id="project-section-a">
 
-            <div className="pa-left-container">
-                <div className="pa-left-image-container">
-                    <img className="pa-left-image" src={require("../../Assets/ProjectSectionA/PSALanding.png")} alt="TGGITS"/>
+            <div className="psa-left-container">
+                <div className="psa-left-image-container">
+                    <img className="psa-left-image" src={require("../../Assets/ProjectSectionA/PSALanding.png")} alt="TGGITS"/>
                 </div>
             </div>
 
-            <div className="pa-right-container">
-                <div className="pa-info-container">
-                    <div className="pa-title">
+            <div className="psa-right-container">
+                <div className="psa-info-container">
+                    <div className="psa-title">
                         The Great Gig in the Sky
                     </div>
-                    <div className="pa-text">
+                    <div className="psa-text">
                         Add Resume Description
                         <br/>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde molestiae commodi, quo ratione aspernatur provident adipisci, dolore nostrum neque fugiat.
                     </div>
-                    <div className="pa-link-container">
+                    <div className="psa-link-container">
                         <a href="https://github.com/sparklingwaterlemon/The-Great-Gig-in-the-Sky" target="_blank" rel="noreferrer">
-                            <button className="pa-button gh"> GitHub </button>
+                            <button className="psa-button gh"> GitHub </button>
                         </a>
                         <a href="https://the-great-gig-in-the-sky.herokuapp.com/" target="_blank" rel="noreferrer">
-                            <button className="pa-button ll"> Live Link </button>
+                            <button className="psa-button ll"> Live Link </button>
                         </a>
                     </div>
                 </div>
--- src/Components/ProjectSectionB/ProjectSectionB.jsx
+++ src/Components/ProjectSectionB/ProjectSectionB.jsx
@@ -4,30 +4,30 @@ export default function ProjectSectionB(){
     return(
         <section id="project-section-b">
 
-            <div className="pb-left-container">
-                <div className="pb-info-container">
-                    <div className="pb-title">
+            <div className="psb-left-container">
+                <div className="psb-info-container">
+                    <div className="psb-title">
                         Shake On It!
                     </div>
-                    <div className="pb-text">
+                    <div className="psb-text">
                         Add Resume Description
                         <br />
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde molestiae commodi, quo ratione aspernatur provident adipisci, dolore nostrum neque fugiat.
                     </div>
-                    <div className="pb-link-container">
+                    <div className="psb-link-container">
                         <a href="https://github.com/sparklingwaterlemon/Shake-On-It/" target="_blank" rel="noreferrer">
-                            <button className="pb-button gh"> GitHub </button>
+                            <button className="psb-button gh"> GitHub </button>
                         </a>
                         <a href="https://shakeonit.herokuapp.com/" target="_blank" rel="noreferrer">
-                            <button className="pb-button ll"> Live Link </button>
+                            <button className="psb-button ll"> Live Link </button>
                         </a>
                     </div>
                 </div>
             </div>
 
-            <div className="pb-right-container">
-                <div className="pb-right-image-container">
-                    <img className="pb-right-image" src={require("../../Assets/ProjectSectionB/ProjectSectionBLanding.png")} alt="ShakeOnIt"/>
+            <div className="psb-right-container">
+                <div className="psb-right-image-container">
+                    <img className="psb-right-image" src={require("../../Assets/ProjectSectionB/ProjectSectionBLanding.png")} alt="ShakeOnIt"/>
                 </div>
             </div>
         </section>
