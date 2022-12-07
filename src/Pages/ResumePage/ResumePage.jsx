f28d894d575664fe5abb29c443453b03ad7778ef
sparklingwaterlemon <michaelkim009@gmail.com>
2022-12-07 00:06:14 -0800

    Updated New Resume1.3.0

--- src/Pages/ResumePage/ResumePage.jsx
+++ src/Pages/ResumePage/ResumePage.jsx
@@ -3,8 +3,7 @@ import "./ResumePage.css";
 export default function ResumePage(){
     return (
         <section id="resume">            
-            {/* <iframe src={require("../../Assets/ResumeImages/Michael-Kim-Resume-Dec-2022.pdf")} width="100%" height="100%"/> */}
-            <iframe src={require("../../Assets/ResumeImages/Michael-Kim-Resume-Dec-2022.pdf")} title="Mike's Resume" width="100%" height="100%"/>
+            <iframe src={require("../../Assets/ResumeImages/resume1.3.0.pdf")} title="mikes resume" width="100%" height="100%"/>
         </section>
     )
 }
